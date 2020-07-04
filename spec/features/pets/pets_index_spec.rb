e3ad41ace44bfa167a4435feefa1543de692395a
ryn21 <33795357+RyN21@users.noreply.github.com>
2020-07-04 07:45:30 -0600

    Removed shelter from schema.rb by removing db and creating new ones

--- spec/features/pets/pets_index_spec.rb
+++ spec/features/pets/pets_index_spec.rb
@@ -2,13 +2,12 @@ require 'rails_helper'
 
 RSpec.describe 'Pets Index' do
     it "can see list of all pets in the system including attributes" do
-      pet_1 = Pet.create(image: "/app/assets/images/oliver.png", name: "Olvier", age: 4, sex: "male", shelter: "Adopt a Dog")
+      pet_1 = Pet.create(image: "/app/assets/images/oliver.png", name: "Olvier", age: 4, sex: "male")
 
       visit '/pets'
 
 
       expect(page).to have_content(pet_1.name)
       expect(page).to have_content(pet_1.sex)
-      expect(page).to have_content(pet_1.shelter)
   end
 end
